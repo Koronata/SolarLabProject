532cfd56749a89253a1a341b1e6636edc1b492be
Jagiellon <wowerwatch333@gmail.com>
2023-04-08 18:00:29 +0300

    Переделал архитектуру

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import {SettingsComponent} from "./pages/settings/settings.component";
-import {CreateAdvComponent} from "./pages/create-adv/create-adv.component";
-import {MyAdvsComponent} from "./pages/my-advs/my-advs.component";
-import {RecAdsComponent} from "./pages/rec-ads/rec-ads.component";
+
 
 
 
 
 const routes: Routes = [
-  {path: 'settings', component:SettingsComponent},
-  {path: 'create-adv', component:CreateAdvComponent},
-  {path: 'my-advs', component:MyAdvsComponent},
-  {path: 'rec-ads', component:RecAdsComponent}
+
 ]
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,38 +5,19 @@ import {AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {AdvertisementComponent } from './advertisement/advertisement.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SearchComponent } from './shared/components/search/search.component';
-import { CardComponent } from './shared/components/card/card.component';
-import { RecAdsComponent } from './pages/rec-ads/rec-ads.component';
-import { GalleryComponent } from './shared/components/gallery/gallery.component';
-import { CurrencyFormatPipePipe } from './pipes/currency-format.pipe.pipe';
-import {FormsModule} from "@angular/forms";
-import { MyAdvsComponent } from './pages/my-advs/my-advs.component';
-import { CreateAdvComponent } from './pages/create-adv/create-adv.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { ModalRegistrationComponent } from './shared/components/modal-registration/modal-registration.component';
+import { SearchComponent } from './shared/modules/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AdvertisementComponent,
-    HeaderComponent,
     SearchComponent,
-    CardComponent,
-    RecAdsComponent,
-    GalleryComponent,
-    CurrencyFormatPipePipe,
-    MyAdvsComponent,
-    CreateAdvComponent,
-    SettingsComponent,
-    ModalRegistrationComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
